11abc07c7a4b364e2df2cdcf3589c2b1d27451e1
aadi19819 <31537418+aadi19819@users.noreply.github.com>
2026-08-31 15:19:18 +0530

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        var map = new Map();
+
+        for (var str of strs) {
+            //console.log(str);
+            var element = new Array(26).fill(0);
+
+           // console.log(element);
+
+            for (var char of str) {
+                element[char.charCodeAt(0) - 97]++;
+            }
+
+            var key = element.join(",");
+            if (!map.has(key)) {
+                map.set(key, []);
+            } 
+                map.get(key).push(str);
+            
+        }
+        return Array.from(map.values());
+    }
+}
